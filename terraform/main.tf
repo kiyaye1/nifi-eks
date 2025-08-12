82b4b45bff4aa9df1c42be9ba35e4a32d57c0230
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-08-12 12:49:00 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,139 +1,97 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-  default     = "us-east-2"
-}
-
 provider "aws" {
   region = var.aws_region
 }
 
-# Use default VPC + default public subnets
-data "aws_vpc" "default" {
-  default = true
+data "aws_vpc" "default" { default = true }
+
+data "aws_subnets" "default" {
+  filter { name = "vpc-id" values = [data.aws_vpc.default.id] }
 }
 
-data "aws_subnets" "default_public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
+data "aws_subnet" "each" {
+  for_each = toset(data.aws_subnets.default.ids)
+  id       = each.value
 }
 
-# IAM: EKS cluster role
+locals {
+  public_subnet_ids = [ for s in data.aws_subnet.each : s.id if try(s.map_public_ip_on_launch, false) ]
+}
+
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eksClusterRole"
+  name               = "${var.cluster_name}-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
+      Action = "sts:AssumeRole",
       Effect = "Allow",
-      Principal = { Service = "eks.amazonaws.com" },
-      Action = "sts:AssumeRole"
+      Principal = { Service = "eks.amazonaws.com" }
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# EKS cluster
-resource "aws_eks_cluster" "eks" {
-  name     = "eks-ubuntu-cluster"
-  version  = "1.33"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = data.aws_subnets.default_public.ids
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
-}
-
-# IAM: Node role
 resource "aws_iam_role" "eks_node_role" {
-  name = "eksNodeRole"
+  name               = "${var.cluster_name}-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
+      Action = "sts:AssumeRole",
       Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action = "sts:AssumeRole"
+      Principal = { Service = "ec2.amazonaws.com" }
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy" {
+resource "aws_iam_role_policy_attachment" "node_worker" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
-
-resource "aws_iam_role_policy_attachment" "cni_policy" {
+resource "aws_iam_role_policy_attachment" "node_cni" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
-
-resource "aws_iam_role_policy_attachment" "registry_policy" {
+resource "aws_iam_role_policy_attachment" "node_ecr" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Ubuntu 24.04 EKS AMI for 1.33
-data "aws_ssm_parameter" "ubuntu_eks_ami" {
-  name = "/aws/service/canonical/ubuntu/eks/24.04/1.33/stable/current/amd64/hvm/ebs-gp3/ami-id"
-}
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.kubernetes_version
 
-resource "aws_launch_template" "ubuntu_lt" {
-  name_prefix   = "ubuntu-eks-nodes-"
-  image_id      = data.aws_ssm_parameter.ubuntu_eks_ami.value
-  instance_type = "t3.medium"
-
-  user_data = base64encode(<<-EOT
-    #!/bin/bash
-    set -euxo pipefail
-    /etc/eks/bootstrap.sh ${aws_eks_cluster.eks.name}
-  EOT
-  )
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = { Name = "ubuntu-eks-node" }
+  vpc_config {
+    subnet_ids              = local.public_subnet_ids
+    endpoint_public_access  = true
+    endpoint_private_access = false
   }
+
+  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
 }
 
-resource "aws_eks_node_group" "mng" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "ubuntu-node-group"
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-ng"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = data.aws_subnets.default_public.ids
+  subnet_ids      = local.public_subnet_ids
+  instance_types  = [var.node_instance_type]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  launch_template {
-    id      = aws_launch_template.ubuntu_lt.id
-    version = "$Latest"
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.node_policy,
-    aws_iam_role_policy_attachment.cni_policy,
-    aws_iam_role_policy_attachment.registry_policy
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_cni,
+    aws_iam_role_policy_attachment.node_ecr
   ]
 }
 
-output "cluster_name" {
-  value = aws_eks_cluster.eks.name
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
-}
+output "cluster_name"     { value = aws_eks_cluster.this.name }
+output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
