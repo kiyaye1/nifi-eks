9bd6f7d9946224172b6ec1fd17b0b041ad2841c8
kiyaye1 <36045045+kiyaye1@users.noreply.github.com>
2025-08-12 13:13:12 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,35 @@
-variable "aws_region"            { type = string  default = "us-east-2" }
-variable "cluster_name"          { type = string  default = "nifi-eks" }
-variable "kubernetes_version"    { type = string  default = "1.30" }
-variable "node_instance_type"    { type = string  default = "t3.large" }
-variable "node_desired_size"     { type = number  default = 2 }
-variable "node_min_size"         { type = number  default = 1 }
-variable "node_max_size"         { type = number  default = 3 }
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "nifi-eks"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.30"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.large"
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 3
+}
+
